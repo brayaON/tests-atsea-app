039368ea38b78ad202e9237d75cbacefd1549702
CLoaizaF <cloaiza@unal.edu.co>
2021-03-10 19:04:55 -0500

    Configuration of the UI tests done

---
+++ protractor/headless.config.ts
@@ -0,0 +1,34 @@
+import { Config } from 'protractor';
+
+const firefoxConfig = {
+  browserName: 'firefox',
+  name: 'firefox-tests',
+  shardTestFiles: true,
+  maxInstances: 1
+};
+
+const chromeConfig = {
+  browserName: 'chrome',
+  name: 'chrome-tests',
+  shardTestFiles: true,
+  maxInstances: 1
+};
+
+const multiCapabilities = [chromeConfig, firefoxConfig];
+
+export const config: Config = {
+  multiCapabilities,
+  framework: 'mocha',
+  specs: [ '../test/ui/**/*.js' ],
+  seleniumAddress: 'http://0.0.0.0:4444',
+  SELENIUM_PROMISE_MANAGER: false,
+  mochaOpts: {
+	reporter: 'mochawesome-screenshots'
+  },
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu']
+    }
+  }
+};
