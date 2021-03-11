ff25b976e38fcf64f507374b6bac9776e9b51203
brayaON <bortizf@unal.edu.co>
2021-03-11 18:52:01 -0500

    Getting the title of atsea with chai

--- test/ui/buyADockerBaby.test.ts
+++ test/ui/buyADockerBaby.test.ts
@@ -1,26 +1,14 @@
 import { browser } from 'protractor';
 import * as chai from 'chai';
 const expect = chai.expect
-/*
+
 describe('When opening the API Page', () => {
     beforeEach(async () => {
-        await browser.get('http://localhost:8080/index.html#/?_k=zaehma');
+        await browser.get('http://192.168.1.111:8080/');
     });
 
     it('then should have a title', async () => {
-        await expect(browser.getTitle()).toEqual('Atsea Shop');
+      const title = await browser.getTitle();
+      expect(title).to.equal('Atsea Shop');
     });
-});*/
-
-describe('Given a SDET learning protractor', () => {
-    describe('when open Google Page', () => {
-      beforeEach(async () => {
-        await browser.get('http://www.google.com');
-      });
-  
-      it('then should have a title', async () => {
-        const title = await browser.getTitle();
-        expect(title).to.equal('Google');
-      });
-    });
-  });
+});
